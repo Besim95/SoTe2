36595fda151e14995339395f8f366f20558056a8
Besim Duyar <besimduyar@eduroam192036.vpn.rz.htwg-konstanz.de>
2016-11-18 15:23:02 +0100

    Verändert

---
+++ WebContent/EinfacheFunktion.js
@@ -0,0 +1,12 @@
+
+
+
+function hinzufuegen(){
+	
+	var inhalt = document.getElementById("eingabe").value;
+	var veraendert = console.log(inhalt.innerHTML);
+	document.getElementById("test").innerHTML = inhalt;
+	
+	
+	
+}
